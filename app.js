854f05b9b8810a38de10bfc9e5616d3db51f29f5
Parzival-P1 <76760065+Parzival-P1@users.noreply.github.com>
2022-06-04 15:32:51 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+var name  = new Array();
+names[0]  = 'Yakoov';
+names[1]  = 'John';
+names[2]  = 'Jen';
+names[3]  = 'Jason';
+names[4]  = 'Paul';
+names[5]  = 'Frank';
+names[6]  = 'Larry';
+names[7]  = 'Paula';
+names[8]  = 'Laura';
+names[9]  = 'Jim';
+
+for (var i = 0; i < names.length; i++) {
+
+    if (names[i].charAt(0) === 'J' || names[i].charAt(0)==='j') {
+        console.log('Goodbye ' + names[i]);
+} else {
+    console.log('Hello ' + names[i]);
+    }
+} 
